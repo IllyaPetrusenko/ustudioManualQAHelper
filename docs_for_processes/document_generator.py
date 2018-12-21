750bfc33a3b7915ca952fd0f7fccfb684bfe34e0
OCDS Team Lead <illya.petrusenko@gmail.com>
2018-12-21 18:54:28 +0200

    cn_doc_gen file add

--- docs_for_processes/document_generator.py
+++ docs_for_processes/document_generator.py
@@ -1,8 +1,5 @@
-import requests, os, hashlib, json, random
+import requests, os, hashlib, json
 from requests.auth import HTTPBasicAuth
-from docs_for_processes.document_types import planning_dt, tender_dt
-from docs_for_processes.fake_data import word, dfv, dfnv, text
-from pprint import pprint, pformat
 from faker import Faker
 
 
@@ -76,14 +73,3 @@ class DocumentReadyToAttach:
                }
 
 
-
-for t in tender_dt:
-    type_d = t
-    doc = DocumentReadyToAttach(doc_size_kb=1024, file_name=word, doc_format=dfv, document_type=type_d)
-    doc.create_document()
-    doc.calculate_md5_sum()
-    doc.calculate_doc_size()
-    doc.create_payload_4_registration()
-    doc.register_doc()
-    doc.upload_doc()
-    print(json.dumps(doc.docs_2_cn()))
